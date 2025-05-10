a241ca06e73ed2862a029c9f5dcf4bf35ab907d1
Leonardo Racioppi <leoracioppi@outlook.com>
2025-05-10 19:52:35 -0300

    set variables and start to work on functions start and update

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.querySelector('h1').addEventListener('click', function () {
-  document.querySelector('h1').style.color = '#657999';
-});
+const display = document.getElementById('display');
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+}
+
+function stop() {}
+
+function reset() {}
+
+function update() {
+  const curretTime = Date.now();
+  elapsedTime = curretTime = startTime;
+
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  let seconds = Math.floor((elapsedTime / 1000) % 60);
+  let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+}
